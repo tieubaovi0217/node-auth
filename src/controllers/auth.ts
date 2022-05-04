47cbecc8184962d01fe7841f90b4a61a403c7531
tieubaovi0217 <nguyenvitieubao0217@gmail.com>
2022-05-04 15:58:13 +0700

    fix handle error auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -11,37 +11,34 @@ import { Request, Response, NextFunction } from 'express';
 config();
 export default {
   async login(req: Request, res: Response, next: NextFunction) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return next(new ErrorHandler(400, errors.array()[0].msg));
-    }
-
     const { username, password } = req.body;
+    const errors = validationResult(req);
     try {
+      if (!errors.isEmpty()) {
+        throw new ErrorHandler(400, errors.array()[0].msg);
+      }
       const { user, token } = await AuthService.getInstance().login(
         username,
         password,
       );
       res.json({ user, token });
     } catch (err) {
-      next(new ErrorHandler(400, err.message || 'Login failed'));
+      next(err);
     }
   },
 
   async signup(req: Request, res: Response, next: NextFunction) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return next(new ErrorHandler(400, errors.array()[0].msg));
-    }
-
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        throw new ErrorHandler(400, errors.array()[0].msg);
+      }
       const { user, token } = await AuthService.getInstance().signUp(req.body);
 
       await fs.promises.mkdir(makePath(user.username, ''));
       res.json({ user, token });
     } catch (err) {
-      console.error(err);
-      next(new ErrorHandler(400, err.message || 'Signup failed'));
+      next(err);
     }
   },
 };
